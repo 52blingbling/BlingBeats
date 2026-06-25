f3f0b33f664bf79b3ce323be3d0ee2fb8d8d0992
52blingbling <64056319+52blingbling@users.noreply.github.com>
2026-06-25 11:35:48 +0800

    修复导入逻辑

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,7 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.5.0")
 
     implementation("androidx.compose.foundation:foundation")
-    implementation("androidx.document:document:1.0.1")
+    implementation("androidx.documentfile:documentfile:1.1.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
